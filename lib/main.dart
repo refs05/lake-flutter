40a79c386de3e0164006f0a32f417b12b8fe16c0
refs05 <restufajarsidhiq02@gmail.com>
2023-03-16 11:18:16 +0700

    try routing with animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,151 +1,209 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+// void main() => runApp(const MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget titleSection = Container(
+//       padding: const EdgeInsets.all(32),
+//       child: Row(
+//         children: [
+//           Expanded(
+//             /*1*/
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 /*2*/
+//                 Container(
+//                   padding: const EdgeInsets.only(bottom: 8),
+//                   child: const Text(
+//                     'Oeschinen Lake Campground',
+//                     style: TextStyle(
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                 ),
+//                 Text(
+//                   'Kandersteg, Switzerland',
+//                   style: TextStyle(
+//                     color: Colors.grey[500],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           /*3*/
+//           const FavoriteWidget(),
+//         ],
+//       ),
+//     );
+//
+//     Color color = Theme.of(context).primaryColor;
+//
+//     Widget buttonSection = Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//       children: [
+//         _buildButtonColumn(color, Icons.call, 'CALL'),
+//         _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+//         _buildButtonColumn(color, Icons.share, 'SHARE'),
+//       ],
+//     );
+//
+//     Widget textSection = const Padding(
+//       padding: EdgeInsets.all(32),
+//       child: Text(
+//         'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+//             'Alps. Situated 1,578 meters above sea level, it is one of the '
+//             'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+//             'half-hour walk through pastures and pine forest, leads you to the '
+//             'lake, which warms to 20 degrees Celsius in the summer. Activities '
+//             'enjoyed here include rowing, and riding the summer toboggan run.',
+//         softWrap: true,
+//       ),
+//     );
+//
+//     return MaterialApp(
+//       title: 'Lake App',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('My Lake App'),
+//         ),
+//         body: Column(
+//           children: [Image.asset('images/lake.jpg', width: 600, height: 240, fit: BoxFit.cover,), titleSection, buttonSection, textSection],
+//         ),
+//         ),
+//       );
+//   }
+//
+//   Column _buildButtonColumn(Color color, IconData icon, String label) {
+//     return Column(
+//       mainAxisSize: MainAxisSize.min,
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         Icon(icon, color: color),
+//         Container(
+//           margin: const EdgeInsets.only(top: 8),
+//           child: Text(
+//             label,
+//             style: TextStyle(
+//               fontSize: 12,
+//               fontWeight: FontWeight.w400,
+//               color: color,
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+//
+// class FavoriteWidget extends StatefulWidget {
+//   const FavoriteWidget({super.key});
+//
+//   @override
+//   State<FavoriteWidget> createState() => _FavoriteWidgetState();
+// }
+//
+// class _FavoriteWidgetState extends State<FavoriteWidget> {
+//   bool _isFavorited = true;
+//   int _favoriteCount = 41;
+//
+//   void _toggleFavorite() {
+//     setState(() {
+//       if (_isFavorited) {
+//         _favoriteCount -= 1;
+//         _isFavorited = false;
+//       } else {
+//         _favoriteCount += 1;
+//         _isFavorited = true;
+//       }
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisSize: MainAxisSize.min,
+//       children: [
+//         Container(
+//             padding: const EdgeInsets.all(0),
+//             child: IconButton(
+//               padding: const EdgeInsets.all(0),
+//               alignment: Alignment.centerRight,
+//               icon: (_isFavorited? const Icon(Icons.star) : const Icon(Icons.star_border)),
+//               color: Colors.red[500],
+//               onPressed: _toggleFavorite,
+//             )
+//         ),
+//         SizedBox(
+//           width: 18,
+//           child: Text('$_favoriteCount'),
+//         )
+//       ],
+//     );
+//   }
+// }
 
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Page1(),
+    ),
+  );
+}
 
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          const FavoriteWidget(),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
+          child: const Text('Go!'),
+        ),
       ),
     );
+  }
+}
 
-    Color color = Theme.of(context).primaryColor;
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(0.0, 1.0);
+      const end = Offset.zero;
+      const curve = Curves.ease;
 
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
 
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My Lake App'),
-        ),
-        body: Column(
-          children: [Image.asset('images/lake.jpg', width: 600, height: 240, fit: BoxFit.cover,), titleSection, buttonSection, textSection],
-        ),
-        ),
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
       );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+    },
+  );
 }
 
-class FavoriteWidget extends StatefulWidget {
-  const FavoriteWidget({super.key});
-
-  @override
-  State<FavoriteWidget> createState() => _FavoriteWidgetState();
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-
-  void _toggleFavorite() {
-    setState(() {
-      if (_isFavorited) {
-        _favoriteCount -= 1;
-        _isFavorited = false;
-      } else {
-        _favoriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-            padding: const EdgeInsets.all(0),
-            child: IconButton(
-              padding: const EdgeInsets.all(0),
-              alignment: Alignment.centerRight,
-              icon: (_isFavorited? const Icon(Icons.star) : const Icon(Icons.star_border)),
-              color: Colors.red[500],
-              onPressed: _toggleFavorite,
-            )
-        ),
-        SizedBox(
-          width: 18,
-          child: Text('$_favoriteCount'),
-        )
-      ],
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Page 2'),
+      ),
     );
   }
 }
